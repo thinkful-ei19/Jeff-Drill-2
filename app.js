aa110576b589e836ee2c3220a9e9623bf706bb43
Jeff Vega <jeff@intellibliss.com>
2018-02-19 20:18:22 -0800

    day output successful

--- app.js
+++ app.js
@@ -49,3 +49,39 @@ function decode(str){
 var x = "craft block argon meter bells brown croon droop";
 console.log(decode(x))
 
+function day(month,leapyear){
+    var x = (leapyear % 100 === 0) ? (leapyear % 400 === 0) : (leapyear % 4 === 0)
+    if(x === true && month ==="February"){
+        return  `February has 29 days`
+    }
+    else if (x === false &&month ==="February"){
+        return `February has 28 days`
+    }
+    if(month===" "){
+        throw new Error("Must provide a valid month.")
+        
+    }
+   var  y = month
+    switch(y){
+    case'January'|| 1:
+    case 'March'||3:
+    case' May' ||5:
+    case'July'||7 :
+    case'August'||8: 
+    case'October'||10: 
+    case'December'||12:
+    console.log(`${month}  has 31 days`)
+    break;
+    case 'April'||4:
+    case 'June'||6:
+    case ' September'||9:
+    case 'November'||11:
+    console.log(`${month}  has 30 days`)
+    break;
+    }
+    
+    // February - 28 days in a common year and 29 days in leap years
+
+}
+
+console.log(day("January",2020))
